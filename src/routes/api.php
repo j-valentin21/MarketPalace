99d5de877a09d55c80b4d885f7328d36d0c197a5
Joel <jvalentin0221@gmail.com>
2021-02-25 12:56:07 -0500

    Add resource routes to api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('buyers',BuyerController::class,['only' => ['index', 'show']]);
+Route::resource('categories',CategoryController::class,['except' => ['create', 'edit']]);
+Route::resource('products',ProductController::class,['only' => ['index', 'show']]);
+Route::resource('sellers',SellerController::class,['only' => ['index', 'show']]);
+Route::resource('transaction',TransactionController::class,['only' => ['index', 'show']]);
+Route::resource('users',UserController::class,['except' => ['create', 'edit']]);
