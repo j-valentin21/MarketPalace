37419d51d9318123599ae3b5ab412eae574b9f80
Joel <jvalentin0221@gmail.com>
2021-03-02 16:50:11 -0500

    Add global scope to buyer model for transaction queries

--- src/app/Models/Buyer.php
+++ src/app/Models/Buyer.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
+use App\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Buyer extends User
 {
     use HasFactory;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new BuyerScope);
+    }
 
     public function transactions()
     {
