f01114c85f6db28231edfaa4ed1faf7bcd4b5ed5
Joel <jvalentin0221@gmail.com>
2021-03-23 12:32:34 -0400

    Add feature test for TransactionCategoryTest

---
+++ src/tests/Feature/TransactionCategoryTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class TransactionCategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    : void {
+
+        parent::setUp();
+
+        Artisan::call('db:seed');
+    }
+
+    public function test_If_Users_Index_Is_Working_Properly()
+    {
+        $this->json('GET', '/transactions/1/categories', ['Accept' => 'application/json'])
+            ->assertStatus(200);
+    }
+}
