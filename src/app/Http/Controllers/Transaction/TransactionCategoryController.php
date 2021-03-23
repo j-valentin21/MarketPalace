1ea016fdf27a08fd7ddbbb2e5993ad53f2c752c3
Joel <jvalentin0221@gmail.com>
2021-03-23 11:06:45 -0400

    Add TransactionCategoryController to capture relationship between transaction and category

---
+++ src/app/Http/Controllers/Transaction/TransactionCategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Transaction;
+use Illuminate\Http\JsonResponse;
+
+class TransactionCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Transaction $transaction
+     * @return JsonResponse
+     */
+    public function index(Transaction $transaction): JsonResponse
+    {
+        $categories = $transaction->product()->with('categories')->get();
+
+        return $this->showAll($categories);
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Buyer\BuyerController;
 use App\Http\Controllers\Category\CategoryController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionCategoryController;
 use App\Http\Controllers\Transaction\TransactionController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
@@ -20,9 +21,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Buyers
+ */
 Route::resource('buyers',BuyerController::class,['only' => ['index', 'show']]);
+
+/**
+ * Categories
+ */
 Route::resource('categories',CategoryController::class,['except' => ['create', 'edit']]);
+
+/**
+ * Products
+ */
 Route::resource('products',ProductController::class,['only' => ['index', 'show']]);
+
+/**
+ * Sellers
+ */
 Route::resource('sellers',SellerController::class,['only' => ['index', 'show']]);
+
+/**
+ * Transactions
+ */
 Route::resource('transactions',TransactionController::class,['only' => ['index', 'show']]);
+Route::resource('transactions.categories',TransactionCategoryController::class,['only' => 'index']);
+
+
+/**
+ * Users
+ */
 Route::resource('users',UserController::class,['except' => ['create', 'edit']]);
