45be5cd347e039e967455d6db4c5e9d83e3affa3
Joel <jvalentin0221@gmail.com>
2021-03-24 17:40:18 -0400

    Add SellerTransactionController to feature test

--- src/routes/api.php
+++ src/routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Category\CategorySellerController;
 use App\Http\Controllers\Category\CategoryTransactionController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Seller\SellerTransactionController;
 use App\Http\Controllers\Transaction\TransactionCategoryController;
 use App\Http\Controllers\Transaction\TransactionController;
 use App\Http\Controllers\Transaction\TransactionSellerController;
@@ -56,6 +57,7 @@ Route::resource('products',ProductController::class,['only' => ['index', 'show']
  * Sellers
  */
 Route::resource('sellers',SellerController::class,['only' => ['index', 'show']]);
+Route::resource('sellers.transactions',SellerTransactionController::class,['only' => 'index']);
 
 /**
  * Transactions
---
+++ src/tests/Feature/SellerTransactionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Seller;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class SellerTransactionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    : void {
+
+        parent::setUp();
+
+        Artisan::call('db:seed');
+    }
+
+    public function test_If_Seller_Transaction_Index_Is_Working_Properly()
+    {
+        $seller = Seller::all()->random()->id;
+
+        $this->json('GET', '/sellers/' . $seller . '/transactions', ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                [
+                    "id",
+                    "quantity",
+                    "buyer_id",
+                    "product_id",
+                    "created_at",
+                    "updated_at",
+                    "deleted_at",
+                ]
+            ]);
+    }
+}
