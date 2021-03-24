51d4321d777aae36bd534e4abd5f56c799c58f46
Joel <jvalentin0221@gmail.com>
2021-03-24 15:41:11 -0400

    Add CategorySellerController with feature test

---
+++ src/tests/Feature/CategorySellerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class CategorySellerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    : void {
+
+        parent::setUp();
+
+        Artisan::call('db:seed');
+    }
+
+    public function test_If_Category_Product_Index_Is_Working_Properly()
+    {
+        $categories = Category::all()->random()->id;
+
+        $this->json('GET', '/categories/' . $categories . '/sellers', ['Accept' => 'application/json'])
+            ->assertStatus(200);
+    }
+}
