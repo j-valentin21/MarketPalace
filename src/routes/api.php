f52ca8afbfa406c346eb113c641c6c6e20a05e4b
Joel <jvalentin0221@gmail.com>
2021-03-26 13:17:32 -0400

    Add SellerBuyerController with feature test

--- src/routes/api.php
+++ src/routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Category\CategoryProductController;
 use App\Http\Controllers\Category\CategorySellerController;
 use App\Http\Controllers\Category\CategoryTransactionController;
 use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerBuyerController;
 use App\Http\Controllers\Seller\SellerCategoryController;
 use App\Http\Controllers\Seller\SellerController;
 use App\Http\Controllers\Seller\SellerTransactionController;
@@ -60,6 +61,7 @@ Route::resource('products',ProductController::class,['only' => ['index', 'show']
 Route::resource('sellers',SellerController::class,['only' => ['index', 'show']]);
 Route::resource('sellers.transactions',SellerTransactionController::class,['only' => 'index']);
 Route::resource('sellers.categories',SellerCategoryController::class,['only' => 'index']);
+Route::resource('sellers.buyers',SellerBuyerController::class,['only' => 'index']);
 
 /**
  * Transactions
---
+++ src/tests/Feature/SellerBuyerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Seller;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class SellerBuyerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    : void {
+
+        parent::setUp();
+
+        Artisan::call('db:seed');
+    }
+
+    public function test_If_Seller_Buyer_Index_Is_Working_Properly()
+    {
+        $seller = Seller::all()->random()->id;
+
+        $this->json('GET', '/sellers/' . $seller . '/buyers', ['Accept' => 'application/json'])
+            ->assertStatus(200);
+    }
+}
