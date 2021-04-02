25a5a37c4e822da8367d7fcc50222d2883d576af
Joel <jvalentin0221@gmail.com>
2021-04-02 17:15:01 -0400

    Create feature test for delete method for SellerProductController

--- src/tests/Feature/SellerProductTest.php
+++ src/tests/Feature/SellerProductTest.php
@@ -7,6 +7,7 @@ use App\Models\Seller;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class SellerProductTest extends TestCase
@@ -67,4 +68,25 @@ class SellerProductTest extends TestCase
 //            ]);
 //
 //    }
+
+    public function test_Seller_Product_Can_Be_Deleted()
+    {
+        $seller = Seller::all()->random()->id;
+        $products = DB::table('products')->where('seller_id', $seller)->first();
+
+        $this->json('DELETE', '/sellers/' . $seller . "/products/" . $products->id  , ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "id",
+                "name",
+                "description",
+                "quantity",
+                "status",
+                "image",
+                "seller_id",
+                "created_at",
+                "updated_at",
+                "deleted_at"
+            ]);
+    }
 }
