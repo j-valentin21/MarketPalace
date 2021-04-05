45e863e170eaf6a8eaca452f6bdb3bb389ae2fc6
Joel <jvalentin0221@gmail.com>
2021-04-05 12:12:23 -0400

    Add ProductCategoryController with feature tests

---
+++ src/app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Product $product
+     * @return JsonResponse
+     */
+    public function index(Product $product): JsonResponse
+    {
+        $categories = $product->categories;
+
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Product $product
+     * @param Category $category
+     * @return JsonResponse
+     */
+    public function update(Product $product, Category $category): JsonResponse
+    {
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Product $product
+     * @param Category $category
+     * @return JsonResponse
+     */
+    public function destroy(Product $product, Category $category): JsonResponse
+    {
+        if (!$product->categories()->find($category->id)) {
+            return $this->errorResponse('The specified category is not a category of this product', 404);
+        }
+
+        $product->categories()->detach($category->id);
+
+        return $this->showAll($product->categories);
+    }
+}
---
+++ src/tests/Feature/ProductCategoryTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class ProductCategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    : void {
+
+        parent::setUp();
+
+        Artisan::call('db:seed');
+    }
+
+    public function test_If_Product_Transaction_Index_Is_Working_Properly()
+    {
+        $this->json('GET', '/products/' . 1 . '/categories', ['Accept' => 'application/json'])
+            ->assertStatus(200);
+    }
+
+    public function test_If_Product_Category_Can_Be_Updated()
+    {
+        $products = Product::all()->random()->id;
+        $categoryId =   rand(1,25);
+
+        $this->json('PUT', '/products/' . $products . '/categories/' . $categoryId , ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                [
+                    "id",
+                    "name",
+                    "description",
+                    "created_at",
+                    "updated_at",
+                    "deleted_at"
+                ]
+            ]);
+    }
+
+    public function test_If_Product_Category_Responds_With_Status_Code_404_When_Resource_Does_Not_Exist()
+    {
+        $products = Product::all()->random()->id;
+        $categoryId =   rand(50,100);
+
+        $this->json('DELETE', '/products/' . $products . '/categories/' . 31 , ['Accept' => 'application/json'])
+            ->assertStatus(404)
+            ->assertJsonStructure([
+                    "error",
+                    "code"
+            ]);
+    }
+
+    public function test_If_Product_Category_Can_Be_Deleted()
+    {
+        $products = Product::all()->first();
+        $categoryId = $products->categories->pluck('id')->first();
+
+        $this->json('DELETE', '/products/' . $products->id . '/categories/' . $categoryId , ['Accept' => 'application/json'])
+            ->assertStatus(200);
+    }
+}
