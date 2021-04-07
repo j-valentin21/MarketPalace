de48caaf115f06f589cc8d61c13f451fc254f3c2
Joel <jvalentin0221@gmail.com>
2021-04-07 16:47:02 -0400

    add trait method to handle api resources in every request

--- src/app/Traits/ApiResponder.php
+++ src/app/Traits/ApiResponder.php
@@ -2,11 +2,10 @@
 
 namespace App\Traits;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponder
 {
@@ -22,16 +21,35 @@ trait ApiResponder
 
     protected function showAll(Collection $collection, $code = 200): \Illuminate\Http\JsonResponse
     {
+        if ($collection->isEmpty()) {
+            return $this->successResponse(['data' => $collection], $code);
+        }
+        $collection = $this->transformCollectionData($collection);
         return $this->successResponse($collection, $code);
     }
 
     protected function showOne(Model $instance, $code = 200): \Illuminate\Http\JsonResponse
     {
+        $instance = $this->transformResourceData($instance);
         return $this->successResponse($instance, $code);
     }
 
     protected function showMessage($message, $code = 200): \Illuminate\Http\JsonResponse
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function transformCollectionData($data)
+    {
+        $resource = UserResource::collection($data);
+
+        return $resource->toArray($data);
+    }
+
+    protected function transformResourceData($data)
+    {
+        $resource =  new UserResource($data);
+
+        return $resource->toArray($data);
     }
 }
