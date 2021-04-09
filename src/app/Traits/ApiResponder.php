9168fc9ca132e26799753b38b2e4d79c5654f691
Joel <jvalentin0221@gmail.com>
2021-04-09 14:55:53 -0400

    Add code block to allow custom resources per page

--- src/app/Traits/ApiResponder.php
+++ src/app/Traits/ApiResponder.php
@@ -11,6 +11,7 @@ use App\Http\Resources\UserResource;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 trait ApiResponder
@@ -62,9 +63,17 @@ trait ApiResponder
 
     protected function paginate(Collection $collection)
     {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
+
+        Validator::validate(request()->all(), $rules);
 
         $page = LengthAwarePaginator::resolveCurrentPage();
         $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
         $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
         $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
             'path' => LengthAwarePaginator::resolveCurrentPath(),
