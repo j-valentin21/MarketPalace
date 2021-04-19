ced7e3f5e50676d1507d469d3f4c25e51e759a21
Joel <jvalentin0221@gmail.com>
2021-04-19 15:10:02 -0400

    Add policy and middle for product model

---
+++ src/app/Policies/ProductPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ProductPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function addCategory(User $user, Product $product): bool
+    {
+        return $user->id === $product->seller->id;
+    }
+
+    /**
+     * Determine whether the user can delete the product.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Product  $product
+     * @return mixed
+     */
+    public function deleteCategory(User $user, Product $product)
+    {
+        return $user->id === $product->seller->id;
+    }
+}
