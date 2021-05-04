5d50345884ae5b521b0759e5c8379c0a40f65382
Joel <jvalentin0221@gmail.com>
2021-05-04 12:04:55 -0400

    Update product test to pass with updated code

--- src/tests/Feature/product/ProductTest.php
+++ src/tests/Feature/product/ProductTest.php
@@ -2,10 +2,9 @@
 
 namespace Tests\Feature\product;
 
-use App\Models\Product;
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -16,26 +15,5 @@ class ProductTest extends TestCase
     {
         $this->json('GET', '/products', ['Accept' => 'application/json'])
             ->assertStatus(200);
-    }
-
-    public function test_One_Product_Is_Displayed_Based_On_Id()
-    {
-        $user = User::factory()->create();
-        $product = Product::factory()->create();
-
-        $this->json('GET', '/products/' . $user->id , ['Accept' => 'application/json'])
-            ->assertStatus(200)
-            ->assertJsonStructure([
-                "id" ,
-                "name",
-                "description",
-                "quantity",
-                "status",
-                "image",
-                "seller_id",
-                "created_at",
-                "updated_at",
-                "deleted_at",
-            ]);
     }
 }
